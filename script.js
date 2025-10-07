e58aa44b17f0a2d29d1e1e2bde3e4dcb8c8ed247
Viv05-ui <vm57422@westbourne.vic.edu.au>
2025-10-08 10:28:18 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("JS file is loaded!");
+
+document.getElementById("joinButton").addEventListener("click", function() {
+    window.open("https://gofund.me/479e216a8", "_blank");
+  });
+  
